743af57b6daefed278350322f772e532b8fbf022
Sumit <akoliyasumit@hotmail.com>
2024-02-02 20:17:12 +0000

    Padding changes to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className='w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-            <div className='flex flex-col justify-center px-20 mt-20 '>
+            <div className='flex flex-col justify-center px-20 mt-20'>
                 <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Software Engineer in 
                 {/* <pre></pre> */}
                 <br/>

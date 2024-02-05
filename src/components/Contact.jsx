9a53c3f2dfc2b59f9957c26e28d124894a43f371
Sumit <akoliyasumit@hotmail.com>
2024-02-05 21:33:58 +0000

    contacts page padding

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,10 +5,10 @@ const Contact = () => {
     <div
       id="Contact"
       name="contact"
-      className="w-full md:h-screen bg-gradient-to-b from-gray-800 to-black p-4 text-white px-12"
+      className="w-full bg-gradient-to-b from-gray-800 to-black p-4 text-white px-12"
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
-        <div className="pb-8">
+        <div className="pb-8 px-12">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Contact
           </p>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,13 +10,14 @@ const Home = () => {
     <div id="Home" className='w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
             <div className='flex flex-col justify-center px-20 mt-20'>
-                <h2 className='text-4xl sm:text-7xl font-bold text-white pt-5'>I'm a Software Engineer in 
+                <h2 className='text-4xl sm:text-7xl font-bold text-white pt-5'>I'm a Software Engineer with Expertise in 
                 {/* <pre></pre> */}
                 <br/>
-                <h4 className='bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text w-fit inline'>Vanilla JS & </h4>
+                <h4 className='bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text w-fit inline'>NodeJs & </h4>
                 <h4 className='bg-gradient-to-r from-blue-600 via-pink-500 to-indigo-400 text-transparent bg-clip-text w-fit inline'>Java </h4>
+                <h4 className='bg-gradient-to-r from-blue-600 via-pink-500 to-indigo-400 text-transparent bg-clip-text w-fit '>Laravel </h4>
                 </h2>
-                <p>I have 2 years of professional experience</p>
+                <p className='font-bold'>I have 2 years of professional experience</p>
                 
                 <div>
                     <button className='font-bold text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor pointer'>
